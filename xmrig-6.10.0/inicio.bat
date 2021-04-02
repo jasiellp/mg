68640b0964904c88d6f0eee71a3d9619bb9bfc3e
Jasiel Pereira de sant ana <PRO_JPEREIRA@fastalelo.onmicrosoft.com>
2021-04-02 15:25:03 -0300

    Monero

---
+++ xmrig-6.10.0/inicio.bat
@@ -0,0 +1,2 @@
+xmrig.exe --coin monero -o us-west.minexmr.com:4444 -u 861YkYPN7PngJusCb7bLG8cxsWj84cdWpfG8XLkKuy9yd2jiB8N4vQcKbtMiLw5oJRRdghJrzchxygN8m51QGPhBEjjHWNx.002 -p x --donate-level 1
+pause
