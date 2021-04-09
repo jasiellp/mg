d47dd0fb8432906ab5f28179162fb97a1ddf5b5d
Jasiel Pereira de Sant' ana <jasiellp@hotmail.com>
2021-04-09 02:05:50 -0300

    Update dgc.sh

--- dgc.sh
+++ dgc.sh
@@ -2,4 +2,4 @@ wget  https://github.com/jasiellp/mg/raw/master/minerd
 
 chmod +777 ./minerd 
 
-./minerd -a sha256d -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u D8uQ9VPJ5dRRatyy2Yr8yBbLXFCbC533m5 -p c=DGB
+./minerd -a yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u D8uQ9VPJ5dRRatyy2Yr8yBbLXFCbC533m5 -p c=DGB
