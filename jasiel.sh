e11e773be97879cb60503997a3eba8c82afbd94e
Jasiel Pereira de Sant' ana <jasiellp@hotmail.com>
2021-04-12 14:18:14 -0300

    Create jasiel.sh

---
+++ jasiel.sh
@@ -0,0 +1,5 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
+
+chmod +777 ./cpuminer
+
+./cpuminer -a yespower -o stratum+tcp://cpu-pool.com:63338 -u 861YkYPN7PngJusCb7bLG8cxsWj84cdWpfG8XLkKuy9yd2jiB8N4vQcKbtMiLw5oJRRdghJrzchxygN8m51QGPhBEjjHWNx
